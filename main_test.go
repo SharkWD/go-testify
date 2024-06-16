4226299c3f7b1b14ba57a43298f8db76c287076d
SharkWD <SharkWD@yandex.ru>
2024-06-16 16:15:58 +0300

    Изменил структуру проекта и исправил замечания

--- main_test.go
+++ main_test.go
@@ -3,51 +3,12 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandleWhenOk(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
@@ -57,7 +18,7 @@ func TestMainHandleWhenOk(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
+	assert.NotEmpty(t, responseRecorder.Body)
 }
 
 func TestMainHandleWhenNoCity(t *testing.T) {
@@ -69,7 +30,7 @@ func TestMainHandleWhenNoCity(t *testing.T) {
 
 	expected := "wrong city value"
 
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Equal(t, expected, responseRecorder.Body.String())
 }
 
@@ -84,5 +45,6 @@ func TestMainHandleWhenCountMoreThanTotal(t *testing.T) {
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
 
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
 	assert.Len(t, list, totalCount)
 }
